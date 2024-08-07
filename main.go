a6c120b7f7768c818cf3403069aa32e36a8ce9cb
yp2020 <3522373084@qq.com>
2024-08-08 00:07:09 +0800

    初始化项目

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "GoLearnDemo/vessel"
+
+func main() {
+	//vessel.ArrayLearn()
+	//vessel.MultidimensionalArray()
+	//vessel.SliceLearn()
+	//vessel.MapLearn()
+	//vessel.SyncMapLearn()
+	vessel.NilLearn()
+}
